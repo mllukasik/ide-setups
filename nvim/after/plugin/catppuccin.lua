93275d2f6da14a6c47cabcbd9801ff427744013c
rookiebyte <tvpozytyw@gmail.com>
2024-02-06 21:32:19 +0100

    add nvim config

---
+++ nvim/after/plugin/catppuccin.lua
@@ -0,0 +1,7 @@
+require("catppuccin").setup({
+	flavour = "mocha",
+	transparent_background = false,
+})
+
+vim.cmd.colorscheme "catppuccin"
+
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,2 @@
+return { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = {'nvim-lua/plenary.nvim'}}
+
