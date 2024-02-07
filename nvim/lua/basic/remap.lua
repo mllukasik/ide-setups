42d9786edf08f72d2a2809fa83c5588aa62e5fb4
rookiebyte <tvpozytyw@gmail.com>
2024-02-07 08:47:45 +0100

    Lsp zero setup

--- nvim/lua/basic/remap.lua
+++ nvim/lua/basic/remap.lua
@@ -1,3 +1,14 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "w", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-w>", vim.cmd.Ex)
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+
+-- stop p from overwriting buffer
+vim.keymap.set("x", "p", "P", { noremap = true })
+
+-- stop c from overwriting buffer
+vim.keymap.set("v", "c", [["_d]])
 
--- nvim/lua/basic/set.lua
+++ nvim/lua/basic/set.lua
@@ -10,14 +10,21 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 20
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+--vim.opt.colorcolumn = "80"
+
+vim.opt.clipboard="unnamedplus"
 
